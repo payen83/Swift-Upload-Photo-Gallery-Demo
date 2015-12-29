f601ccf0400face241491a723dbd10e4062e9e05
Ferdy Fauzi <ferdyfauzi@gmail.com>
2015-12-29 11:53:57 +0800

    Initial Commit

---
+++ PhotoSaveDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoSaveDemo
+//
+//  Created by Ferdy Fauzi on 29/12/2015.
+//  Copyright © 2015 Ferdy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
